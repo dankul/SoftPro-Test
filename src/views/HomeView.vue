00f519bf5ed156f5c72314f97d00769292b94f30
Kulakovsky Daniil <dk@empat.tech>
2025-05-08 13:32:34 +0300

    Add card list to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,71 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="container">
-  
+    <h1 class="text-4xl font-bold text-center my-8">Welcome to My Real Estate App</h1>
+    <p class="text-lg text-center mb-8">Find your dream property with ease.</p>
+    <Filter @get-search-query="search" />
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <template v-if="isLoading">
+        <SkeletonCard v-for="n in 8" :key="n" />
+      </template>
+      <template v-else>
+        <PropertyCard v-for="property in properties" :key="property.id" :property="property" />
+      </template>
+    </div>
+    <Pagination
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @prev-page="prevPage"
+        @next-page="nextPage"
+      />
   </div>
 </template>
+
+<script setup>
+  import {computed, onMounted, ref} from "vue";
+  import { usePropertyStore  } from '@/stores/property.js';
+  import PropertyCard from "@/components/PropertyCard.vue";
+  import Pagination from "@/components/Pagination.vue";
+  import SkeletonCard from "@/components/SkeletonCard.vue";
+  import Filter from "@/components/Filter.vue";
+  
+  const store = usePropertyStore();
+  const currentPage = ref(store.currentPage);
+  const totalPages = computed(() => store.totalPages);
+  const properties = computed(() => store.paginatedProperties);
+  const isLoading = ref(true);
+  
+  onMounted(() => {
+    loadProperties()
+  })
+  
+  const prevPage = () => {
+    if (currentPage.value > 1) {
+      currentPage.value--;
+      store.setPage(currentPage.value);
+    }
+    
+    loadProperties()
+  };
+  
+  const nextPage = () => {
+    if (currentPage.value < totalPages.value) {
+      currentPage.value++;
+      store.setPage(currentPage.value);
+    }
+    
+    loadProperties()
+  };
+  
+  const loadProperties = () => {
+    isLoading.value = true;
+    setTimeout(() => {
+      store.setPage(currentPage.value);
+      isLoading.value = false;
+    }, 1000);
+  };
+  
+  const search = (query) => {
+    store.setSearchQuery(query);
+    loadProperties()
+  }
+</script>
---
+++ src/views/PropertyView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="relative bg-black">
+    <img
+        :src="image"
+        alt="Property Image"
+        class="w-full h-[40vh] object-cover mb-20 opacity-50"
+    />
+    <h1 class="absolute text-5xl text-white font-bold left-1/2 top-1/2 transform -translate-y-1/2 -translate-x-1/2 whitespace-nowrap">{{ property.name }}</h1>
+  </div>
+  <div class="container">
+    <div class="flex gap-5">
+      <img
+          :src="image"
+          alt="Property Image"
+          class="w-2/5"
+      />
+      <div>
+        <p class="text-2xl">{{ property.name }}</p>
+        <p class="text-xl">{{ property.location }}</p>
+        <p class="text-md"><strong>Price:</strong> {{ property.price }}$</p>
+      </div>
+    </div>
+    <p class="text-lg mt-20 md:w-3/5 mx-auto">{{property.description}}</p>
+    <div class="w-full h-[400px] mt-20" ref="mapContainer"></div>
+  </div>
+</template>
+      
+<script setup>
+import {onMounted, ref} from "vue";
+  import { useRoute } from "vue-router";
+  import  image from 'assets/property-image.jpg'
+  import { usePropertyStore  } from '@/stores/property.js';
+import maplibregl from "maplibre-gl";
+  
+  const route = useRoute();
+  const store = usePropertyStore();
+  const propertyId = route.params.propertyId;
+  const property = store.getSingleProperty(propertyId);
+  const mapContainer = ref(null);
+console.log(property);
+  onMounted(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer.value,
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+      center: property.geometry.coordinates,
+      zoom: 10,
+    });
+    
+    setTimeout(() => {
+      map.resize();
+    }, 100);
+    
+    const marker = new maplibregl.Marker()
+        .setLngLat(property.geometry.coordinates)
+        .addTo(map);
+  });
+</script>
