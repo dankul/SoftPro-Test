95ed675db7a027846b0876dbe9d9aaabc65eb28f
Kulakovsky Daniil <dk@empat.tech>
2025-05-08 15:00:09 +0300

    Add smooth scroll to top on property load in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -59,6 +59,10 @@
   const loadProperties = () => {
     isLoading.value = true;
     setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // Плавная прокрутка
+      });
       store.setPage(currentPage.value);
       isLoading.value = false;
     }, 1000);
