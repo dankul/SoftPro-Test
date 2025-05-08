b12d814f232e1a43c56d0540b517b1b9073ca2db
Kulakovsky Daniil <dk@empat.tech>
2025-05-08 13:30:34 +0300

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="bg-gray-700">
+    <nav class="flex justify-center py-4 px-2">
+      <ul class="flex gap-4">
+        <li class="text-2xl text-white hover:text-blue-500 transition duration-300"><router-link to="/">Home</router-link></li>
+        <li class="text-2xl text-white hover:text-blue-500 transition duration-300"><router-link to="/map">Map</router-link></li>
+        <li class="text-2xl text-white hover:text-blue-500 transition duration-300"><router-link to="/about">About Me</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
