c14d11f46e1d2942f7b8d354cada7a9fdfe6f096
Kulakovsky Daniil <dk@empat.tech>
2025-05-08 09:03:31 +0300

    Init and setup project

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MapView from '../views/MapView.vue'
+import LocationView from "@/views/LocationView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/map',
+      name: 'Map',
+      component: MapView,
+    },
+    {
+      path: '/location/:locationId',
+      name: 'Location',
+      component: LocationView,
+    },
+  ],
+})
+
+export default router
