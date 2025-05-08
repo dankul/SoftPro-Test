dc8989184a522b8ca590c56d168bed2e3fc1d8d2
Kulakovsky Daniil <dk@empat.tech>
2025-05-08 13:33:35 +0300

    Add map page

---
+++ src/views/MapView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="w-full h-screen relative" ref="mapContainer"></div>
+</template>
+
+<script setup>
+import "maplibre-gl/dist/maplibre-gl.css";
+import maplibregl from 'maplibre-gl';
+import { onMounted, ref } from 'vue';
+import { usePropertyStore } from '@/stores/property.js';
+import { useRouter } from 'vue-router';
+
+const mapContainer = ref(null);
+const store = usePropertyStore();
+const router = useRouter();
+
+onMounted(() => {
+  const map = new maplibregl.Map({
+    container: mapContainer.value,
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    center: [30.5234, 50.4501],
+    zoom: 10,
+  });
+  
+  setTimeout(() => {
+    map.resize();
+  }, 100);
+  
+  store.properties.forEach((property) => {
+    const popup = new maplibregl.Popup({closeButton: false, closeOnClick: false})
+        .setHTML(`<h3>${property.name}</h3>`)
+        .setLngLat(property.geometry.coordinates);
+    
+    const marker = new maplibregl.Marker()
+        .setLngLat(property.geometry.coordinates)
+        .setPopup(popup)
+        .addTo(map);
+    
+    const el = marker.getElement();
+    
+    el.addEventListener('mouseenter', () => {
+      marker.getPopup().addTo(map);
+    });
+    
+    el.addEventListener('mouseleave', () => {
+      marker.getPopup().remove();
+    });
+    
+    el.addEventListener('click', () => {
+      router.push(`/property/${property.id}`);
+    });
+  })
+});
+</script>
+
+<style>
+  .maplibregl-marker {
+    cursor: pointer;
+  }
+</style>
